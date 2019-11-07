0eb50487d6433ace1815a8a64a0ca0d8ec550a93
MRloll <37027417+MRloll@users.noreply.github.com>
2019-11-07 23:29:25 +0200

    till featurd courses slider

--- js/app.js
+++ js/app.js
@@ -11,23 +11,47 @@ $(document).ready(function () {
         //adjusting the height of nav on scroll
         if ($(window).innerWidth() > 991) {
             if($(window).scrollTop() > $('.top-nav').height()) {
+                
+                //adjusting the slider top marging when scroll
+                //to prevent a proplem when the nav links takes position fixed
+                $('.slider').css({
+                    marginTop: $('.nav-links').height()
+                });
 
                 $('nav .nav-links li.link').css("padding", "6px 0");
             } else {
     
+                $('.slider').css({
+                    marginTop: 0
+                })
+    
                 $('nav .nav-links li.link').css("padding", "30px 0");
+            }
+        } else if ($(window).innerWidth() < 991) {
+
+            if($(window).scrollTop() > $('.top-nav').height()) {
+                $('.slider').css({
+                    marginTop: $('nav').height()
+                });
+
+            } else {
+                $('.slider').css({
+                    marginTop: 0
+                });    
             }
         }
         
      
         // making the nav sticky to the top
         if($(window).scrollTop() > $('.top-nav').height()) {
-        
+            
+            // give nav links position fixed on scroll
             $('nav').css({
                 position: 'fixed',
                 top: 0
             });
         } else {
+
             $('nav').css({
                 position: 'relative',
             });
@@ -74,12 +98,61 @@ $(document).ready(function () {
 
     });
 
+    /*
+    **********************
+    ** card courses overlay animation
+    **********************
+    */
+    $('.course .course-item .card-img-top').hover(function () {
+
+        $(this).find('.overlay')
+            .animate({width: '100%'}, 300)
+            .animate({width: '90%'}, 100)
+            .animate({width: '100%'}, 100)
+
+    }, function () {
+
+        $(this).find('.overlay')
+            .animate({width: "0%"}, 500);
+    });
 
 
 
 
 
+    new WOW().init();
 
-
+    $('.owl-carousel').slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        nextArrow: '<i class="fa fa-arrow-right"></i>',
+        prevArrow: '<i class="fa fa-arrow-left"></i>',      
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+              breakpoint: 400,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]        
+    })
 
 });
